5a55aafbf612198680b4474299b0a7aec86c8829
Mannatgupta666 <mannat_g@mt.iitr.ac.in>
2026-05-23 13:59:47 +0530

    small fixes

--- api.py
+++ api.py
@@ -54,4 +54,4 @@ if __name__ == "__main__":
     import uvicorn
     import os
     port = int(os.environ.get("PORT", 10000))
-    uvicorn.run("api:app", host="0.0.0.0", port=port)
+    uvicorn.run("api:app", host="0.0.0.0", port=port,  log_level="info")
